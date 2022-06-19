d38dcd21c5b8cdee24e93b957be887dab88db647
Alina <px-mew@mail.ru>
2022-06-19 21:15:14 +0300

    Реализован стреляющий корабль и движение пришельцев

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,32 @@
+import pygame
+from pygame.sprite import Group
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+    # Инициализирует игру и создает объект экрана.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    # Создание корабля.
+    ship = Ship(ai_settings, screen)
+    # Создание группы для хранения пуль.
+    bullets = Group()
+    # Создание группы пришельцев. 
+    aliens = Group()
+    # Создание флота пришельцев.
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+
+    # Запуск основного цикла игры.
+    while True:
+        # Отслеживание событий клавиатуры и мыши.
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+
+run_game()
